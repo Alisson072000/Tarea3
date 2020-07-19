4d7cb1c53bdc83eda1b088856bc59eee73382e1f
Alisson072000 <67445588+Alisson072000@users.noreply.github.com>
2020-07-19 01:36:54 -0500

    Add files via upload

---
+++ Tarea2/Panaderia.cs
@@ -0,0 +1,37 @@
+﻿namespace Tarea2
+{
+    class Panaderia
+    {
+        int id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+ 
+
+        private Empleado trabajador;
+        public Empleado Trabajador
+        {
+            get { return trabajador; }
+            set
+            {
+                trabajador = value;
+                trabajador.Trabaja = this;
+            }
+        }
+
+        public string Informacion()
+        {
+            return $"La Panaderia {Nombre}" +
+                $" esta ubicada {Direccion} puedes contactarnos {Telefono} ";
+        }
+        public string Producir()
+        {
+            return $"La Panaderia {Nombre} produce varios tipos de panes semanales\n";
+        }
+        public string Producir(string parametro)
+        {
+            return $"La Panaderia {Nombre} produce varios tipos de panes mensuales\n";
+        }
+
+    }
+}
